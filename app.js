03b49506f604c8f6c4fb0658dc360de907f1a157
felipeverones <felipe_verones@hotmail.com>
2022-12-08 15:49:18 -0300

    main: criando endpoint de login

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
-var express = require ('express'); 
-//aqui declaramos uma variável que contem o módulo express, que estamos pegando na pasta node_modules
+var express = require ('express');
 
 var app = express();
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.use(express.json());
+
+// Criar endpoint de login, que valida se as credenciais do usuário são válidas.
+app.post('/login', function (req, res) {
+
+    const { userId, password } = req.body;
+    
+    if (!userId || !password) {
+        return res.status(400).json("É necessário informar userId e password.")
+    }
+
+    // Verificar se userId e password existem array abaixo.
+    const credentials = [
+        {userId: 1, password: '123'},
+        {userId: 2, password: '345' }
+    ];
+
+    let authorized = false;
+
+    credentials.forEach(credential => {
+        if (credential.userId == userId && credential.password == password ) {
+            authorized = true;
+        }
+    });
+
+    if (authorized) {
+        return res.status(200).json({
+            status: 200,
+            message: "Usuário autenticado com sucesso."
+        });
+    }
+    return res.status(400).json({
+        status: 400,
+        message: "Não foi possível se autenticar. Verifique id e senha."
+    });
+    
 });
 
-// Endpoint de testes.
 app.get('/verones', function (req, res) {
-
     let val1 = 50;
     let val2 = 100;
-
     res.send('' + (val1+val2));
 });
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
-// listen inicia um servidor, e colocamos ele para escutar a porta 3000. Ele responde com "Hello World!"
