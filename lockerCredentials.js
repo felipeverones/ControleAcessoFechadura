a7ef76be035889d78d21f3ae7f054fb4cfb6e6fa
felipeverones <felipe_verones@hotmail.com>
2022-12-08 14:22:03 -0300

    seg

---
+++ lockerCredentials.js
@@ -0,0 +1,12 @@
+
+
+const lockerData = [
+    {
+        "lockerID": 1,
+        "password": "1234"  
+    },
+    {
+        "lockerID": 2,
+        "password": "4567"  
+    }
+]
---
+++ userCredentials.js
@@ -0,0 +1,11 @@
+
+const loginData = [
+    {
+        "userID": 1,
+        "password": "123"  
+    },
+    {
+        "userID": 2,
+        "password": "456"  
+    }
+]
